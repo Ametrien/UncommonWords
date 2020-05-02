c1e256f0455efa288bfd4308ff01bdee50bc6f47
Ametrien <43112986+Ametrien@users.noreply.github.com>
2020-05-03 01:10:22 +0300

    Create replaceWords.py

---
+++ replaceWords.py
@@ -0,0 +1,19 @@
+import re
+
+lEng = ['one', 'two', 'three']
+lRus = ['один', 'два', 'три']
+
+lUnited = []
+
+for i in range(0, len(lEng)):
+    lEng[i] = '' + lEng[i] + ''
+    lRus[i] = lEng[i] + ' (' + lRus[i] + ')'
+    i += 1
+
+
+print(lRus)
+word_list = dict(zip(lEng, lRus))
+
+with open('book.txt') as main, open('book_upd.txt', 'w') as done:
+    text = main.read()
+    done.write(re.sub(r'\b\w+\b', lambda x: word_list.get(x.group(), x.group()), text))
