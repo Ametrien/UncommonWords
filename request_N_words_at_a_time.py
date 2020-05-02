cf65078a5f04531eb3508da02dcb3baacaa2926f
Ametrien <43112986+Ametrien@users.noreply.github.com>
2020-05-02 12:18:57 +0300

    Update request_N_words_at_a_time.py

--- request_N_words_at_a_time.py
+++ request_N_words_at_a_time.py
@@ -1,12 +1,22 @@
 import requests
 import re
 import sys
-from itertools import islice
+
+class Logger(object):
+    def __init__(self, filename="Default.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+sys.stdout = Logger("words.txt")  # the file is stored in the project directory
+
 
 # Yandex API
-KEY = 'trnsl.1.1.20200501T155620Z.29a16adc41056341.662c3a723031b520e3647e7b0602c6cb5e399214'
-URL = "yourYandexTranslateAPIkey"
-
+KEY = 'yourYandexKey'
+URL = "https://translate.yandex.net/api/v1.5/tr.json/translate"
 
 def translate_me(mytext):
     params = {
@@ -18,7 +28,7 @@ def translate_me(mytext):
     return response.json()
 
 
-document_text = open('/Users/user/Desktop/text.txt', 'r') # your source file with a book
+document_text = open('/Users/username/Desktop/text.txt', 'r')  # your source file with a book
 text_string = document_text.read().lower()
 match_pattern = re.findall(r'\b[a-z]{3,25}\b', text_string)
 
@@ -30,51 +40,34 @@ for word in match_pattern:
 
 frequency_list = frequency.keys()
 
-limit = 5
+lEng = [[k] for k, v in frequency.items() if v > 900]
+allEnglishWords = list(frequency.keys())
 
-my_list = [[k] for k, v in frequency.items() if v < limit]
-englishWords = list(frequency.keys())
 
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-list_of_lists = list(chunks(englishWords, 10)) #10 words at a time, you can change this value
+print(len(lEng))
+list_of_lists = list(chunks(lEng, 30))
+
+s = ''
 
 for list in list_of_lists:
-          json = translate_me(list)
-          print(' '.join(json["text"]))
+    json = translate_me(list)
+    russianWords = ' '.join(json["text"]) + ' '
+    s += russianWords
+    lRus = s.split()
 
+# for j in range(0, len(lRus)):
+#     print(lRus[j]).encode('utf-8').strip()
 
-# diction = {}
+for i in range(0, len(lEng)):
+    # print(lEng[i])
+    lEng[i] = '"' + ', '.join(lEng[i]) + '";'
+    lRus[i] = '"' + lRus[i] + '";'
+    tog = ''
+    tog = lEng[i] + lRus[i]
+    print(tog).encode('utf-8').strip()
+    i += 1
 
-# print(len(englishWords))
-#
-# for i in range(0, len(my_list)):
-#     json = translate_me(my_list[i])
-#     a = '"'+' '.join(json["text"])+'";'
-#     diction['russian'] = a
-#     # print(a)
-#     b = my_list[i]
-#     temp = ""
-#     for j in b:
-#         if j.isalpha():
-#             b = '"'+"".join([temp, j])+'"\n'
-#     diction['english'] = b
-#     print(b)
-#     i += 1
-#     s = ''
-#     s = (a+b).encode('utf-8').strip()
-#     print(s)
-#
-#     class Logger(object):
-#         def __init__(self, filename="Default.log"):
-#             self.terminal = sys.stdout
-#             self.log = open(filename, "a")
-#
-#         def write(self, message):
-#             self.terminal.write(message)
-#             self.log.write(message)
-#
-#     sys.stdout = Logger("words.txt") # the file is stored in the project directory
-#
